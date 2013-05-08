b39211db74f2c4c404f253923e4d5e5a5d74e012
gbtan <gbtan1988@gmail.com>
2013-05-08 23:47:02 +0800

    add res_message check

--- handler/user.py
+++ handler/user.py
@@ -14,19 +14,21 @@ class LoginHandler(BaseHandler):
             self.send_json(result, code)
             return
         res_code = int(self.get_argument('res_code'))
-        if res_code == 0:
+        res_message = self.get_argument('res_message')
+        if res_code == 0 and res_message == 'Success':
             access_token = self.get_argument('access_token')
             open_id = int(self.get_argument('open_id'))
+            expires_in = int(self.get_argument('expires_in'))
             sns = ctrl.user.get_sns(open_id)
             if sns:
                 uid,uname = sns.uid,sns.name
             else:
                 user = ctrl.user.add_user()
                 uid,uname = user.id,user.name
-                ctrl.user.add_snsbind(uid=uid,sid=open_id,access_token=access_token)
+                ctrl.user.add_snsbind(uid=uid,sid=open_id,access_token=access_token,expires=expires_in)
                 ctrl.note.add_category(uid,names=['工作笔记','生活笔记','我的笔记'])
             session_id = self.get_session_id()
-            ctrl.session.set(session_id, {'uid':uid,'siteid':'189','sid':open_id,'uname':uname}, 0)
+            ctrl.session.set(session_id, {'uid':uid,'siteid':'189','sid':open_id,'uname':uname}, expires_in)
         self.send_json(result, code)
 
 class LogoutHandler(BaseHandler):
